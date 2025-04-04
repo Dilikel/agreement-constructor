ae7f20cb08c4e0474022d9d4c323086d6ff27641
vovan101207 <shiryayev_vova@bk.ru>
2025-04-05 01:25:44 +0300

    2.5

--- app/stores/agreement.js
+++ app/stores/agreement.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useAgreementStore = defineStore('AgreementStore', {
 	state: () => ({
 		agreement: {
-			type: '',
+			type: 'person',
 			data: [],
 		},
 	}),
